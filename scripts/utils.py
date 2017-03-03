eda5d1c5e911b1cd73f272608e5d86e9301b6aee
Dirk von Grünigen <dirk@vongruenigen.com>
2017-03-03 14:33:02 +0100

    added a script for preprocessing the cornell movie dialogue dataset

---
+++ scripts/utils.py
@@ -0,0 +1,20 @@
+#
+# BA ML FS17 - Dirk von Grünigen & Martin Weilenmann
+#
+# Description: This module contains the Model class which
+#              is responsible for creating the tensorflow
+#              graph, i.e. building the bidirectional LSTM
+#              network used in this project.
+#
+
+import sys
+
+from os import path
+
+SOURCE_PATH = path.realpath(path.join(path.dirname(__file__), '..', 'source'))
+
+def expand_import_path_to_source():
+    '''This method ensures that, when called, we can import modules
+       from the source/ directory when using a script.'''
+    if SOURCE_PATH not in sys.path:
+        sys.path.insert(0, SOURCE_PATH)
--- source/data_loader.py
+++ source/data_loader.py
@@ -8,7 +8,17 @@
 
 class DataLoader(object):
     '''This class is responsible for loading and preprocessing
-       the training and test data used in this project.'''
+       the training and test data used in this project. This class
+       can load data in the specified format: The conversations have
+       to be fully expaned (e.g. by using the script preprocess_cornell_movie_dialogues_dataset.py).
+       In the dialogue files, each dialogue should be written down one turn per line. This means that
+       a dialogue where each participant says two sentences has four lines in complete. Each dialogue
+       has to be finished by a special token (defined in the SPLIT_CONV_SYM constant).'''
+
+    # NOTE: All datasets have to be preprocessed again if this symbol
+    #       changes, otherwise the DataLoader class won't be able to
+    #       load the conversations!
+    SPLIT_CONV_SYM = '<<<<<END-CONV>>>>>'
 
     def __init__(self, cfg):
         '''Constructor of the DataLoader class. It only expects
