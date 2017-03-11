added new models and (probably) fixed the problem with the sampled softmax

multimodel/model_old.py
@@ -11,9 +11,11 @@ import utils
 import logger
 import math
 import tensorflow as tf
+import tf.rnn.rnn_cell as rnn
 import numpy as np
 
-from tensorflow.contrib import rnn, seq2seq, layers
+from tensorflow.contrib import seq2seq, layers
+
 from config import Config
 
 from models import Seq2Seq
@@ -281,7 +283,7 @@ class Model(object):
                     embeddings=self.embeddings,
                     start_of_sequence_id=Config.EOS_WORD_IDX,
                     end_of_sequence_id=Config.EOS_WORD_IDX,
-                    maximum_length=tf.reduce_max(self.encoder_inputs_length) + 3,
+                    maximum_length=tf.reduce_max(self.encoder_inputs_length) ,#+ 3,
                     num_decoder_symbols=self.__get_vocab_size()                    
                 )
             else:
@@ -292,7 +294,7 @@ class Model(object):
                     embeddings=self.embeddings,
                     start_of_sequence_id=Config.EOS_WORD_IDX,
                     end_of_sequence_id=Config.EOS_WORD_IDX,
-                    maximum_length=tf.reduce_max(self.encoder_inputs_length) + 3,
+                    maximum_length=tf.reduce_max(self.encoder_inputs_length) ,#+ 3,
                     num_decoder_symbols=self.__get_vocab_size()
                 )
 

multimodel/runner.py
@@ -15,12 +15,11 @@ import json
 import logger
 import tqdm
 
+import seq2seq, memn2n
+
 from os import path
 from config import Config
 from data_loader import DataLoader
-from model import Base
-from seq2seq import Seq2Seq
-
 class Runner(object):
     '''This class is responsible for the training,
        testing and running of the model.'''
@@ -39,6 +38,8 @@ class Runner(object):
             raise Exception('cfg_path must be either a path or a Config object')
 
         logger.init_logger(self.cfg)
+        logger.debug('The following config will be used')
+        logger.debug(json.dumps(self.cfg.cfg_obj, indent=4, sort_keys=True))
 
         self.__prepare_summary_writers()
         self.__prepare_results_directory()
@@ -70,6 +71,7 @@ class Runner(object):
             sum_losses = 0.0
             sum_iters = 0
             batches_per_epoch = self.cfg.get('batches_per_epoch')
+            metrics_track = []
             loss_track = []
             perplexity_track = []
 
@@ -84,8 +86,8 @@ class Runner(object):
             verbose=True
 
             batches = utils.random_sequences(length_from=length_from, length_to=length_to,
-                                       vocab_lower=vocab_lower, vocab_upper=vocab_upper,
-                                       batch_size=batch_size)
+                                             vocab_lower=vocab_lower, vocab_upper=vocab_upper,
+                                             batch_size=batch_size)
 
             training_batches = batches
 
@@ -99,7 +101,7 @@ class Runner(object):
                     for batch in tqdm.tqdm(range(batches_per_epoch+1)):
                         # Prepare the training data
                         batch_data_x, batch_data_y = self.__prepare_data_batch(training_batches)
-                        feed_dict = model.make_train_inputs(batch_data_x, batch_data_x)
+                        feed_dict = model.make_train_inputs(batch_data_x, batch_data_y)
                         
                         metric_ops_dict = model.get_metric_ops()
                         metric_names = list(metric_ops_dict.keys())
@@ -112,41 +114,16 @@ class Runner(object):
 
                         metrics_results = {metric_names[i]: results[i] for i, x in enumerate(metric_names)}
 
-                        import pdb
-                        pdb.set_trace()
-
-                        sum_losses += loss
+                        sum_losses += metrics_results['loss']
                         sum_iters += 1
                         perplexity = np.exp(sum_losses / sum_iters)
                         
-                        loss_track.append(loss)
+                        loss_track.append(metrics_results['loss'])
                         perplexity_track.append(perplexity)
+                        metrics_track.append(metrics_results)
 
-                    logger.info('[Finished Epoch #%i]' % epoch_nr)
-                    logger.info('    Loss       > %f' % loss_track[-1])
-                    logger.info('    Perplexity > %f' % perplexity_track[-1])
-                    
-                    if self.cfg.get('show_predictions_while_training'):
-                        logger.info('Last three samples with predictions:')
-                        for i, (e_in, dt_exp, dt_pred) in enumerate(zip(
-                            feed_dict[model.encoder_inputs].T,
-                            feed_dict[model.decoder_targets].T,
-                            session.run(model.get_inference_op(), feed_dict).T
-                        )):
-                            # NOTE: WTF do we have to subtract one?!
-                            dt_pred = list(map(lambda x: x - 1, dt_pred))
-
-                            text_in = self.data_loader.convert_indices_to_text(e_in, self.rev_vocabulary)
-                            text_exp = self.data_loader.convert_indices_to_text(dt_exp, self.rev_vocabulary)
-                            text_out = self.data_loader.convert_indices_to_text(dt_pred, self.rev_vocabulary)
-
-                            logger.info('[Sample #%i of epoch #%i]' % (i+1, epoch_nr))
-                            logger.info('    Input      > %s' % text_in)
-                            logger.info('    Prediction > %s' % text_out)
-                            logger.info('    Expected   > %s' % text_exp)
-                            
-                            # don't show more than three samples per epoch
-                            if i >= 2: break
+                    self.__print_epoch_state(metrics_track[-1], epoch_nr)
+                    self.__show_samples_at_end_of_epoch(model, session, feed_dict, epoch_nr)
 
                     # Store model after one epoch
                     self.__store_model(session, model, epoch_nr)
@@ -182,7 +159,7 @@ class Runner(object):
            or inference.'''
         with tf.device(self.__get_device()):
             with self.__with_tf_session() as session:
-                model = Seq2Seq(self.cfg, session)
+                model = self.__create_model(session)
 
                 # Build and initialize the graph
                 model.build()
@@ -305,6 +282,54 @@ class Runner(object):
             raise Exception('__prepare_results_directory() must be called before using __get_model_path()')
 
         return path.join(self.curr_exp_path, 'model-%s.chkp' % str(version))
+
+    def __print_epoch_state(self, metrics, epoch_nr):
+        '''Prints the metrics after an epoch has been finished.'''
+        logger.info('[Finished Epoch #%i]' % epoch_nr)
+
+        max_len_name = max(map(lambda x: len(x), metrics.keys()))
+
+        for k, v in metrics.items():
+            logger.info('  %s= %f' % (k.ljust(max_len_name+1), float(v)))
+
+    def __show_samples_at_end_of_epoch(self, model, session, feed_dict, epoch_nr):
+        if self.cfg.get('show_predictions_while_training'):
+            logger.info('Last three samples with predictions:')
+
+            for i, (e_in, dt_exp, dt_pred) in enumerate(zip(
+                feed_dict[model.encoder_inputs].T,
+                feed_dict[model.decoder_targets].T,
+                session.run(model.get_inference_op(), feed_dict).T
+            )):
+                # NOTE: WTF do we have to subtract one?!
+                dt_pred = list(map(lambda x: x - 1, dt_pred))
+
+                text_in = self.data_loader.convert_indices_to_text(e_in, self.rev_vocabulary)
+                text_exp = self.data_loader.convert_indices_to_text(dt_exp, self.rev_vocabulary)
+                text_out = self.data_loader.convert_indices_to_text(dt_pred, self.rev_vocabulary)
+
+                logger.info('[Sample #%i of epoch #%i]' % (i+1, epoch_nr))
+                logger.info('  Input      > %s' % text_in)
+                logger.info('  Prediction > %s' % text_out)
+                logger.info('  Expected   > %s' % text_exp)
+                
+                # don't show more than three samples per epoch
+                if i >= 2: break
+
+    def __create_model(self, session):
+        '''Creates a model based on the name given.'''
+        name = self.cfg.get('model_name')
+        model_ctor_map = {
+            'seq2seq': seq2seq.Seq2Seq,
+            'memn2n': memn2n.MemN2N
+        }
+
+        if name not in model_ctor_map:
+            logger.fatal('the model with the name %s does not exist\n'
+                         'the following are available: '
+                         ', '.join(model_ctor_map.keys()))
+
+        return model_ctor_map[name](self.cfg, session)
 
     def __prepare_data_batch(self, all_data):
         '''Returns two lists, each of the size of the configured batch size. The first contains

multimodel/seq2seq.py
@@ -1,10 +1,10 @@
 #
 # BA ML FS17 - Dirk von Grünigen & Martin Weilenmann
 #
-# Description: This module contains the model class which defines a model
-#              as seen in the paper: https://arxiv.org/pdf/1511.06807.pdf.
-#              The implementation itself is heavily on the implementation
-#              by domluna at https://github.com/domluna/memn2n.
+# Description: This module contains the Model class which
+#              is responsible for creating the tensorflow
+#              graph, i.e. building the bidirectional LSTM
+#              network used in this project.
 #
 
 import utils
@@ -39,7 +39,7 @@ class Seq2Seq(model.Base):
     def __init__(self, cfg, session):
         '''Constructor for the seq2seq model.'''
         self.s2s_cfg = self.DEFAULT_PARAMS.copy()
-        self.s2s_cfg.update(cfg.get('seq2seq'))
+        self.s2s_cfg.update(cfg.get('model_config'))
         self.cell_fn = self.CELL_FN[self.s2s_cfg.get('cell_type')]
         super(Seq2Seq, self).__init__(cfg, session)
 
@@ -47,7 +47,7 @@ class Seq2Seq(model.Base):
         return self._train_op
 
     def get_metric_ops(self):
-        return {'loss': self._loss}
+        return {'loss': self._loss, 'perplexity': self._perplexity}
 
     def get_inference_op(self):
         return self._inference_op
@@ -232,7 +232,12 @@ class Seq2Seq(model.Base):
     def __init_decoder(self):
         '''Initializes the decoder part of the model.'''
         with tf.variable_scope('decoder') as scope:
-            output_fn = lambda outs: layers.linear(outs, self.__get_vocab_size(), scope=scope)
+            def output_fn_f(outs):
+                import pdb
+                pdb.set_trace()
+                return layers.linear(outs, self.__get_vocab_size(), scope=scope)
+
+            output_fn = output_fn_f #lambda outs: layers.linear(outs, self.__get_vocab_size(), scope=scope)
 
             if self.cfg.get('use_attention'):
                 attention_states = tf.transpose(self.encoder_outputs, [1, 0, 2])
@@ -288,7 +293,7 @@ class Seq2Seq(model.Base):
                     scope=scope
                 )
 
-            self.decoder_logits_train = output_fn(self.decoder_outputs_train)
+            self.decoder_logits_train = self.decoder_outputs_train
             self.decoder_prediction_train = tf.argmax(self.decoder_logits_train, axis=-1, name='decoder_prediction_traion')
 
             scope.reuse_variables()
@@ -308,47 +313,42 @@ class Seq2Seq(model.Base):
         logits = tf.transpose(self.decoder_logits_train, [1, 0, 2])
         targets = tf.transpose(self.decoder_train_targets, [1, 0])
         softmax_loss_fn = None
-
+        
         # If there are more words than configured in 'max_vocabulary_size',
         # we start using sampled softmax, otherwise the training process won't
         # fit on a single GPU without problems.
         if self.__get_vocab_size() > self.cfg.get('max_vocabulary_size'):
-            max_vocabulary_size = self.cfg.get('max_vocabulary_size')
+            target_vocab_size = self.cfg.get('max_vocabulary_size')
             num_hidden_units = self.cfg.get('num_hidden_units')
             num_sampled = self.cfg.get('sampled_softmax_number_of_samples')
             
-            w_t = tf.get_variable('out_proj_w', [max_vocabulary_size, num_hidden_units], dtype=tf.float32)
-            w   = tf.transpose(w_t)
-            b   = tf.get_variable('out_proj_b', [max_vocabulary_size], dtype=tf.float32)
+            w = tf.get_variable('out_proj_w', [num_hidden_units, target_vocab_size], dtype=tf.float32)
+            w_t = tf.transpose(w)
+            b = tf.get_variable('out_proj_b', [target_vocab_size], dtype=tf.float32)
+            
+            self.output_projection = (w, b)
 
-            def sampled_softmax_loss(labels, inputs):
-              labels = tf.reshape(labels, [-1, 1])
+            def sampled_softmax_loss(inputs, labels):
+                labels = tf.reshape(labels, [-1, 1])
 
-              # We need to compute the sampled_softmax_loss using 32bit floats to
-              # avoid numerical instabilities.
-              local_w_t = tf.cast(w_t, tf.float32)
-              local_b = tf.cast(b, tf.float32)
-              local_inputs = tf.cast(inputs, tf.float32)
+                # We need to compute the sampled_softmax_loss using 32bit floats to
+                # avoid numerical instabilities.
+                local_w_t = tf.cast(w_t, tf.float32)
+                local_b = tf.cast(b, tf.float32)
+                local_inputs = tf.cast(inputs, tf.float32)
 
-              return tf.cast(
-                  tf.nn.sampled_softmax_loss(
-                      weights=local_w_t,
-                      biases=local_b,
-                      labels=labels,
-                      inputs=local_inputs,
-                      num_sampled=num_sampled,
-                      num_classes=max_vocabulary_size))
+                return tf.nn.sampled_softmax_loss(
+                        local_w_t, local_b,
+                        labels, local_inputs,
+                        num_sampled, target_vocab_size)
 
-            # Currently disabled because of an issue with tensorflow, probably
-            # going to implemente it by hand in case there's no solution..
-            # softmax_loss_fn = sampled_softmax_loss
-
-        # Track the global step state when training
-        self._global_step = tf.Variable(0, name='global_step', trainable=False)
+            softmax_loss_fn = sampled_softmax_loss
 
         self._loss = seq2seq.sequence_loss(logits=logits, targets=targets,
                                            weights=self.loss_weights,
                                            softmax_loss_function=softmax_loss_fn)
+
+        self._perplexity = tf.pow(2.0, self._loss)
 
         self._train_op = tf.train.AdadeltaOptimizer().minimize(self._loss, global_step=self._global_step)
 

multimodel/utils.py
@@ -6,6 +6,7 @@
 
 import numpy as np
 import pickle
+import collections
 
 from os import path
 from gensim.models import Word2Vec
@@ -40,18 +41,20 @@ def reverse_vocabulary(vocabulary):
 def prepare_embeddings_and_vocabulary(embeddings, vocabulary):
     '''Adds an embeddings for an unknown word at the top of the embeddings
        matrix and updates the vocabulary accordingly.'''
-    unknown_embedding = np.random.uniform(-1.0, 1.0, size=(1, embeddings.shape[1]))
-    embeddings = np.vstack([unknown_embedding, embeddings])
+    unknown_emb = np.random.uniform(-1.0, 1.0, size=(1, embeddings.shape[1]))
+    pad_emb = np.random.uniform(-1.0, 1.0, size=(1, embeddings.shape[1]))
+    eos_emb = np.random.uniform(-1.0, 1.0, size=(1, embeddings.shape[1]))
 
-    # NOTE: The +3 when setting the index comes from the fact
-    #       that the embedding for the unknown, eos and pad word
-    #       will be inserted as the first rows of the embeddings
-    #       matrix in order to keep it simple.
-    new_vocabulary = {w: idx+3 for w, idx in vocabulary.items()}
+    # embeddings = np.vstack([unknown_emb, pad_emb, eos_emb, embeddings])
 
-    new_vocabulary[Config.UNKNOWN_WORD_TOKEN] = Config.UNKNOWN_WORD_IDX
-    new_vocabulary[Config.PAD_WORD_TOKEN] = Config.PAD_WORD_IDX
-    new_vocabulary[Config.EOS_WORD_TOKEN] = Config.EOS_WORD_IDX
+    # NOTE: The +3 when setting the index comes from the
+    #       fact that the embedding for the unknown, eos, pad words
+    #       will be inserted as the first row of the embeddings.
+    new_vocabulary = {w: idx for w, idx in vocabulary.items()}
+
+    # new_vocabulary[Config.UNKNOWN_WORD_TOKEN] = Config.UNKNOWN_WORD_IDX
+    # new_vocabulary[Config.PAD_WORD_TOKEN] = Config.PAD_WORD_IDX
+    # new_vocabulary[Config.EOS_WORD_TOKEN] = Config.EOS_WORD_IDX
 
     return embeddings.astype('float32'), new_vocabulary
 