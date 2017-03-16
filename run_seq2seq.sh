755a525077ff41a7feddec15313d660f106f41f8
Dirk von Grünigen <dirk@vongruenigen.com>
2017-03-16 11:41:05 +0100

    added a basic script for using the seq2seq tool

---
+++ run_seq2seq.sh
@@ -0,0 +1,2 @@
+cd seq2seq && python -m bin.train --config_paths="../configs/seq2seq/nmt_middle.yml,../configs/seq2seq/train_seq2seq.yml,../configs/seq2seq/input_pipeline/opensubtitles.yml" \
+                                  --output_dir="../results/seq2seq/"; cd ..
