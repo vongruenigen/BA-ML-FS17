295a48b0485f2ffe9734ec26f5554983d92e9b2b
Dirk von Grünigen <dirk@vongruenigen.com>
2017-03-19 13:55:10 +0100

    added configs for using seq2seq for a first few experiments

--- run_seq2seq.sh
+++ run_seq2seq.sh
@@ -1,2 +1,12 @@
-cd seq2seq && python -m bin.train --config_paths="../configs/seq2seq/nmt_middle.yml,../configs/seq2seq/train_seq2seq.yml,../configs/seq2seq/input_pipeline/opensubtitles.yml" \
-                                  --output_dir="../results/seq2seq/"; cd ..
+SEQ2SEQ_DIR_NAME="seq2seq"
+
+if [ ! -f $SEQ2SEQ_DIR_NAME ]; then
+  echo "Cloning google/seq2seq project from github and store it in $SEQ2SEQ_DIR_NAME..."
+  git clone https://github.com/google/seq2seq.git $SEQ2SEQ_DIR_NAME
+fi
+
+cd $SEQ2SEQ_DIR_NAME
+LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda-8.0/extras/CUPTI/lib64 \
+python -m bin.train --config_paths="../configs/seq2seq/nmt_middle_256.yml,../configs/seq2seq/train_seq2seq.yml,../configs/seq2seq/input_pipeline/opensubtitles.yml" \
+                    --output_dir="../results/seq2seq-opus-2016/"
+cd ..
