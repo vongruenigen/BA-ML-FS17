3fc9a7c6c7d3c6ebacda52a7c3bc53c4710269a1
Dirk von Grünigen <dirk@vongruenigen.com>
2017-03-20 17:27:01 +0100

    try to fix problems related to library loading in docker

--- run_seq2seq.sh
+++ run_seq2seq.sh
@@ -14,7 +14,7 @@ fi
 export S2S_PREFIX='../configs/seq2seq'
 
 cd $SEQ2SEQ_DIR_NAME
-LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda-8.0/extras/CUPTI/lib64 \
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda-8.0/extras/CUPTI/lib64 \
 python -m bin.train --config_paths="$S2S_PREFIX/nmt_4096.yml,$S2S_PREFIX/train_seq2seq.yml,\
                                     $S2S_PREFIX/input_pipeline/opensubtitles.yml,$S2S_PREFIX/metrics.yml" \
                     --output_dir="../results/seq2seq-opus-2016/"
