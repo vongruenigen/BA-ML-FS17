f78083325e969cb5dcf5d77553e3ccbb383ccdff
Dirk von Grünigen <dirk@vongruenigen.com>
2017-03-20 17:17:31 +0100

    add missing space to run_dockerized.sh

--- run_dockerized.sh
+++ run_dockerized.sh
@@ -2,4 +2,4 @@
 docker build -t weilemar-vongrdir-ba-ml-fs17 .
 nvidia-docker run -it -v /cluster/home/weilemar/data/farm-ai/images/BA-ML-FS17:/BA-ML-FS17 \
                   --device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl \
-                  --device /dev/nvidia-uvm:/dev/nvidia-uvmweilemar-vongrdir-ba-ml-fs17 /BA-ML-FS17/run_seq2seq.sh
+                  --device /dev/nvidia-uvm:/dev/nvidia-uv mweilemar-vongrdir-ba-ml-fs17 /BA-ML-FS17/run_seq2seq.sh
