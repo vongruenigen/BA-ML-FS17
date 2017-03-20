12c45f83d2566377d0076eaa1fedc897a3514d3e
Dirk von Grünigen <dirk@vongruenigen.com>
2017-03-20 18:02:26 +0100

    fix a problem related to gpu mapping between host and docker

--- run_dockerized.sh
+++ run_dockerized.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-docker build -t weilemar-vongrdir-ba-ml-fs17 .
-nvidia-docker run -it -v /cluster/home/weilemar/data/farm-ai/images/BA-ML-FS17:/BA-ML-FS17 \
-                  --device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl \
-                  --device /dev/nvidia-uvm:/dev/nvidia-uvm weilemar-vongrdir-ba-ml-fs17 /BA-ML-FS17/run_seq2seq.sh
+# docker build -t weilemar-vongrdir-ba-ml-fs17 .
+nvidia-docker run -it -v /cluster/home/weilemar/data/farm-ai/images/BA-ML-FS17:/weilemar-vongrdir-ba-ml-fs17 \
+                  weilemar-vongrdir-ba-ml-fs17 /BA-ML-FS17/run_seq2seq.sh
