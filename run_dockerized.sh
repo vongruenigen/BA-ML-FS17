fc741ee30be853929f5502216224a7d0079abbda
Dirk von Grünigen <dirk@vongruenigen.com>
2017-03-20 15:59:06 +0100

    try to install dependencies via docker execute before starting the container

--- run_dockerized.sh
+++ run_dockerized.sh
@@ -1,2 +1,4 @@
 docker build -t ba-ml-fs17 .
+docker exec vongrdir-weilemar-ba-ml-fs17 "pip install cython"
+docker exec vongrdir-weilemar-ba-ml-fs17 "pip install -r /BA-ML-FS17/requirements.txt"
 nvidia-docker run -it -v /cluster/home/weilemar/data/farm-ai/images/BA-ML-FS17:/BA-ML-FS17 "$@"
