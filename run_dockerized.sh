5b85ed0ca1d8ebe9bd9fd56a4f50bb260a1d4c2e
Dirk von Grünigen <dirk@vongruenigen.com>
2017-03-20 19:28:43 +0100

    do not always build the container, do it by hand instead

--- run_dockerized.sh
+++ run_dockerized.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# docker build -t weilemar-vongrdir-ba-ml-fs17 .
 nvidia-docker run -it \
                   -p 8750:8008 \
                   -v /cluster/home/weilemar/data/farm-ai/images/BA-ML-FS17:/BA-ML-FS17 \
