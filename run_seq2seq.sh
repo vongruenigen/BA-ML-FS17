43da89e638b2eeb9a2b95e501aead77dace225e7
Dirk von Grünigen <dirk@vongruenigen.com>
2017-03-23 11:18:55 +0100

    capture all output of the experiments in logs/tf.log

--- run_seq2seq.sh
+++ run_seq2seq.sh
@@ -17,10 +17,13 @@ mkdir -p $OUT_PATH
 # Run tensorboard in the background
 python -m tensorflow.tensorboard --port=8008 --logdir=$OUT_PATH &2> tensorboard.log
 
+# Log everything to stderr
+set -x
+
 cd $SEQ2SEQ_DIR_NAME
 LD_PRELOAD="/usr/lib/libtcmalloc_minimal.so.4" \
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda-8.0/extras/CUPTI/lib64 \
 python -m bin.train --config_paths="$S2S_PREFIX/nmt_2048.yml,$S2S_PREFIX/train_seq2seq.yml,\
                                     $S2S_PREFIX/input_pipeline/opensubtitles.yml,$S2S_PREFIX/metrics.yml" \
-                    --output_dir=$OUT_PATH
+                    --output_dir=$OUT_PATH 2> logs/tf.log
 cd ..
