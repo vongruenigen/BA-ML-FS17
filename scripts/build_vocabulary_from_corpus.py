1e62ff777c5c299670b1b92212dec5a576009080
Dirk von Grünigen <dirk@vongruenigen.com>
2017-03-28 19:25:34 +0200

    added script for generating a vocabulary based on a given corpus

---
+++ scripts/build_vocabulary_from_corpus.py
@@ -0,0 +1,49 @@
+#
+# BA ML FS17 - Dirk von Grünigen & Martin Weilenmann
+#
+# Description: Generates a list of tokens from the given
+#              corpus which can be used as the vocabulary.
+#
+
+import sys
+import os
+import pickle
+
+from nltk import word_tokenize
+
+argv = sys.argv[1:]
+
+if len(argv) < 2:
+    print('ERROR: Mandatory arguments missing')
+    print('       (e.g. python scripts/build_vocabulary_from_corpus.py <corpus-in> <voc-out>')
+    sys.exit(2)
+
+corpus_in = argv[0]
+voc_out = argv[1]
+
+vocab_dict = {}
+
+for i, line in enumerate(open(corpus_in, 'r')):
+    # Skip type declarations
+    if i == 0 and line.startswith('#'):
+        continue
+
+    words = word_tokenize(line.lower().strip('\n'))
+
+    for w in words:
+        if w in vocab_dict:
+            vocab_dict[w] = vocab_dict[w] + 1
+        else:
+            vocab_dict[w] = 1
+
+    if (i+1) % 10000 == 0:
+        print('Processed %i lines...' % (i+1))
+
+words_out = []
+word_freq_tuples = list(vocab_dict.items())
+word_freq_tuples.sort(reverse=True, key=lambda x: x[1])
+words_out = list(map(lambda x: x[0], word_freq_tuples))
+
+with open(voc_out, 'w+') as f:
+    for w in words_out:
+        f.write('%s\n' % w)
