248652ff1e1b63482b569d6757e838432deaceb9
Dirk von Grünigen <dirk@vongruenigen.com>
2017-04-06 01:52:38 +0200

    use the current working directory instead of a fixed path

--- run_dockerized.sh
+++ run_dockerized.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 nvidia-docker run -it \
-                  -v /cluster/home/weilemar/data/farm-ai/images/BA-ML-FS17:/BA-ML-FS17 \
+                  -v `pwd`:/BA-ML-FS17 \
                   weilemar-vongrdir-ba-ml-fs17 /BA-ML-FS17/run.sh "$@"
