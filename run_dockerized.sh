0956b485135b799dc1a190153cdcdc56800e8e05
Dirk von Grünigen <dirk@vongruenigen.com>
2017-04-06 02:12:25 +0200

    readd the missing "-gpu" part to the tensorflow dependency

--- run_dockerized.sh
+++ run_dockerized.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 nvidia-docker run -it \
                   -v `pwd`:/BA-ML-FS17 \
-                  weilemar-vongrdir-ba-ml-fs17 /BA-ML-FS17/run.sh "$@"
+                  ba-ml-fs17 /BA-ML-FS17/run.sh "$@"
