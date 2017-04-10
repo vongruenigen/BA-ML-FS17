f3e7bb36d515df972ce8b0edd9d33cb492f372c5
Dirk von Grünigen <dirk@vongruenigen.com>
2017-04-10 20:41:03 +0200

    "dockerize" the run_web.sh script

--- run_web.sh
+++ run_web.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-python source/web/app.py
+# Check that all required arguments are present
+if [[ ! $# -eq 1 ]] ; then
+    echo 'ERROR: Missing mandatory arguments!'
+    echo '       (./run_web_inference.sh <port>)'
+    exit 2
+fi
+
+PORT=$1
+
+while :; do
+  nvidia-docker run -it -p $PORT:9001 -v /cluster/home/weilemar/data/farm-ai/images/BA-ML-FS17:/BA-ML-FS17 \
+                     weilemar-vongrdir-ba-ml-fs17 python /BA-ML-FS17/source/web/app.py
+  sleep 1
+done
