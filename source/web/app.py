5b97fcf1ba4a0c249875f0936a09ab0e48ffbc43
Dirk von Grünigen <dirk@vongruenigen.com>
2017-04-10 20:48:55 +0200

    change ü to ue since the server apparently has problems with it..

--- source/web/app.py
+++ source/web/app.py
@@ -1,5 +1,5 @@
 #
-# BA ML FS17 - Dirk von Grünigen & Martin Weilenmann
+# BA ML FS17 - Dirk von Gruenigen & Martin Weilenmann
 #
 # Description: This module is responsible for running a
 #              smallish web application which can be used
